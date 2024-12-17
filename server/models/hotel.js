38493eb83c9b7dc08916452043bd3c6431f7342a
aida <aidad9628@gmail.com>
2024-12-17 17:20:20 +0000

    ajout

--- server/models/hotel.js
+++ server/models/hotel.js
@@ -1,73 +1,53 @@
 
-// // models/hotel.js
-// const mongoose = require('mongoose');
-
-// const hotelSchema = new mongoose.Schema({
-//     name: { 
-//         type: String, 
-//         required: true,
-//         trim: true 
-//     },
-//     description: { 
-//         type: String, 
-//         default: '' 
-//     },
-//     email: { 
-//         type: String, 
-//         required: true,
-//         lowercase: true, 
-//         trim: true 
-//     },
-//     tel: { 
-//         type: String, 
-//         required: true 
-//     },
-//     pricePerNight: { 
-//         type: Number, 
-//         required: true, 
-//         min: 1 
-//     },
-//     currency: { 
-//         type: String, 
-//         default: 'EUR' 
-//     },
-//     address: { 
-//         type: String, 
-//         required: true 
-//     },
-//     imageUrl: { 
-//         type: String, 
-//         default: 'https://via.placeholder.com/300x200' 
-//     },
-//     userId: { 
-//         type: mongoose.Schema.Types.ObjectId, 
-//         ref: 'User',
-//         default: null 
-//     }
-// }, {
-//     timestamps: true
-// });
-
-// module.exports = mongoose.model('Hotel', hotelSchema);
-
-
-
 // models/hotel.js
 const mongoose = require('mongoose');
 
 const hotelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  tel: { type: String, required: true },
-  pricePerNight: { type: Number, required: true },
-  currency: { type: String, default: 'EUR' },
-  imageUrl: { type: String, default: 'https://via.placeholder.com/300x200' },
-  address: { type: String, required: true },
-  userId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
-  }
-}, { timestamps: true });
+    name: { 
+        type: String, 
+        required: true,
+        trim: true 
+    },
+    description: { 
+        type: String, 
+        default: '' 
+    },
+    email: { 
+        type: String, 
+        required: true,
+        lowercase: true, 
+        trim: true 
+    },
+    tel: { 
+        type: String, 
+        required: true 
+    },
+    pricePerNight: { 
+        type: Number, 
+        required: true, 
+        min: 1 
+    },
+    currency: { 
+        type: String, 
+        default: 'EUR' 
+    },
+    address: { 
+        type: String, 
+        required: true 
+    },
+    imageUrl: { 
+        type: String, 
+        default: 'https://via.placeholder.com/300x200' 
+    },
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        default: null 
+    }
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Hotel', hotelSchema);
+
+
