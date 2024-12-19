993f7d49f6c69302b65bfab71c43f18b5268940a
aida <aidad9628@gmail.com>
2024-12-19 10:05:29 +0000

    Modifier le fichier server

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const hotelRoutes = require("./routes/hotelRoutes"); 
 
-const app = express();
-app.use(cors());
+const APP = express();
+APP.use(cors());
 
-app.use(express.json());
-app.use(session({
+APP.use(express.json());
+APP.use(session({
     secret: 'votre_secret',
     resave: false,
     saveUninitialized: true,
@@ -23,14 +23,14 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => console.error("MongoDB connection error:", error));
 
 
-app.use("/api", authRoutes);
-app.use("/api", hotelRoutes); 
+APP.use("/api", authRoutes);
+APP.use("/api", hotelRoutes); 
 
-app.use((req, res) => {
+APP.use((req, res) => {
   res.status(404).send('<h1>404! Page not found</h1>');
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+APP.listen(PORT, () => {
   console.log(`Server listening at port: ${PORT}`);
 });
